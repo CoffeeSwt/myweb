80aac2d163650b328fe93dad0664eca14ead91cc
coffee_sw <897884964@qq.com>
2023-06-13 13:58:48 +0800

    ✨ feat: add router and pinia

---
+++ src/routes/routes.ts
@@ -0,0 +1,12 @@
+export const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/pages/Home.vue"),
+  },
+  //   {
+  //     path: "/text",
+  //     name: "text",
+  //     component: () => import("@/pages/Text.vue"),
+  //   },
+];
