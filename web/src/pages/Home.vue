231c776f4ea7eeeab6086e69ed12c0e939688eb7
coffee_sw <897884964@qq.com>
2023-06-13 15:20:29 +0800

    🐳 chore:Rebuild directory structure

---
+++ web/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { MdPreview } from "md-editor-v3";
+// preview.css相比style.css少了编辑器那部分样式
+import "md-editor-v3/lib/preview.css";
+
+const id = "preview-only";
+const text = ref("# Hello Editor");
+</script>
+
+<template>
+  <div class="bg-slate-300">
+    <MdPreview :editorId="id" :modelValue="text" />
+  </div>
+</template>
+
+<style scoped></style>
