d290534a76aadb400c9bffecd9c6f9feaabe4acd
coffee_sw <897884964@qq.com>
2023-06-29 03:23:14 +0800

    🐞 fix: 测试env

--- backend/nest/dist/config/jwt.config.js
+++ backend/nest/dist/config/jwt.config.js
@@ -6,7 +6,7 @@ exports.default = (0, config_1.registerAs)('jwt', () => {
         secret: '' + process.env.JWT_SECRET,
         audience: '' + process.env.JWT_TOKEN_AUDIENCE,
         issuer: '' + process.env.JWT_TOKEN_ISSUER,
-        accessTokenTtl: parseInt('' + process.env.JWT_ACCESS_TOKEN_TTL ?? '3600', 10),
+        accessTokenTtl: 3600,
     };
 });
 //# sourceMappingURL=jwt.config.js.map
