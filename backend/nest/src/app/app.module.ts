134400133dc22bdf146065971263a264374b18ba
coffee_sw <897884964@qq.com>
2023-06-29 12:45:11 +0800

    🐞 fix: 更新数据库

--- backend/nest/src/app/app.module.ts
+++ backend/nest/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TypegooseModule } from 'nestjs-typegoose';
   imports: [
     UserModule,
     AuthModule,
-    TypegooseModule.forRoot('mongodb://root:120400@localhost:27017/?authMechanism=DEFAULT'),
+    TypegooseModule.forRoot('mongodb://root:120400@localhost:27017/myweb?authMechanism=DEFAULT&authSource=admin'),
     // TypegooseModule.forRoot('mongodb://localhost:27017/myweb'),
     ConfigModule.forRoot({
       isGlobal: true,
